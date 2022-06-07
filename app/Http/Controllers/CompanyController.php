8d3cae2808d3dad81e7e828092d020d76d0bede6
Fidel Tamondong <genesfidelt@gmail.com>
2022-06-07 13:08:51 +0800

    Adds in the initial Controllers

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function update(Request $request, Company $company)
+    {
+
+    }
+
+    public function destroy(Company $company)
+    {
+        
+    }
+}
